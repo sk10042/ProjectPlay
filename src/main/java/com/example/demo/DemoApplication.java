386c53c7ac2e8839a68a0ec374417a036aea1847
angularjs80 <bigcen@naver.com>
2017-11-16 18:39:33 +0900

    CORS 필터 추가 및 Application.java 에서 해당 필터링을 사용할수 있도록 서블릿관련 추가 작성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,20 @@
 package com.example.demo;
 
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.WebApplicationInitializer;
+
+import com.example.demo.config.CORSFilter;
 
 @Configuration
 @ComponentScan(basePackages="com.example.demo")
@@ -14,8 +22,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 
 @SpringBootApplication
-@EnableScheduling
-public class DemoApplication {
+//@EnableScheduling
+
+
+
+public class DemoApplication  extends SpringBootServletInitializer implements WebApplicationInitializer{
+
+	@SuppressWarnings("restriction")
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		FilterRegistration.Dynamic corsFilter = servletContext.addFilter("corsFilter", CORSFilter.class);
+		corsFilter.addMappingForUrlPatterns(null, false, "/*");
+
+		System.out.println("#########################################servletContext#############################");		
+		System.out.println(servletContext);
+		//super.onStartup(servletContext);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
